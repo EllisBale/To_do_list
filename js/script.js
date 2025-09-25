93325e2a251c865e230058aee43601fbe42ba3eb
EllisBale <elscar29@gmail.com>
2025-09-25 20:42:37 +0100

    Add task creation function with click event listener

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList")
 
+
+let tasks = [];
+
+// Add Task
+addTaskBtn.addEventListener("click", () => {
+    const taskText = taskInput.value.trim();
+    if (taskText === ""){
+        alert("Please enter a task!")
+        return;
+    }
+
+    const task = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    };
+
+    tasks.push(task);
+    renderTasks();
+    saveTasks();
+    
+    taskInput.value = "";
+});
